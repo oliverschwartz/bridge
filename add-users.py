a8794a20e4c91be5756847a66e2e9431dd9710b2
Oliver Schwartz <ollieschwartz99@gmail.com>
2018-06-26 21:14:26 +1000

    add-users.py

---
+++ add-users.py
@@ -0,0 +1,14 @@
+# POST calls iterating through each sub account
+# user fields:
+# email
+# first
+# last
+# practice
+# role (manager, practice_admin, learner)
+def add(subAccount, user):
+
+
+
+
+if __name__ == "__main__":
+    # iterate through .csv file and each user
